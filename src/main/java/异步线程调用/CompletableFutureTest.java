3070f7988fcc6f6f31281f3bb4446ec5da1cfe49
Zepp.Deng <361321200@qq.com>
2021-03-08 11:15:29 +0800

    CompletableFuture串行，同步执行伪代码

--- src/main/java/异步线程调用/CompletableFutureTest.java
+++ src/main/java/异步线程调用/CompletableFutureTest.java
@@ -17,17 +17,23 @@ import java.util.concurrent.CompletableFuture;
 public class CompletableFutureTest {
 
     public static void main(String[] args) throws InterruptedException {
-        runAsync();
+        //模拟执行异步任务并获取结果
+        supplyAsync();
+        //模拟多个CompletableFuture串行执行
+        serialAsync();
+        //模拟多个CompletableFuture并行执行
+        parallelAsync();
     }
 
     /**
      * @Description:
+     *          场景一：异步调用，获取返回结果后继续处理
      *          runAsync方法不支持返回值。
      *          supplyAsync可以支持返回值。
      * @Author Zepp Deng [2021-03-07 22:09]
      * @throws InterruptedException
      */
-    public static void runAsync() throws InterruptedException {
+    public static void supplyAsync() throws InterruptedException {
         // 创建异步执行任务
         CompletableFuture<Double> completableFuture = CompletableFuture.supplyAsync(CompletableFutureTest::fetchPrice);
         // 如果执行成功
@@ -43,15 +49,116 @@ public class CompletableFutureTest {
 
     /**
      * @Description:
-     *          模拟执行任务
+     *          模拟异步调用执行任务
      * @Author Zepp Deng [2021-03-07 22:10]
      * @Return Double
      */
-    public static Double fetchPrice(){
+    private static Double fetchPrice(){
         try {
             Thread.sleep(100);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+        if (Math.random() < 0.3) {
+            throw new RuntimeException("查询价格失败!");
+        }
+        return 5 + Math.random() * 20;
+    }
+
+    /**
+     * @Description:
+     *          场景二：第一个任务执行成功后串行执行第二个任务
+     * @Author Zepp Deng [2021-03-08 10:37]
+     */
+    public static void serialAsync() throws InterruptedException {
+
+        //第一个任务
+        CompletableFuture<String> cfQuery = CompletableFuture.supplyAsync(()-> queryCode("中国石油"));
+        //第一个任务成功后串行执行第二个
+        CompletableFuture<Double> cfFetch = cfQuery.thenApplyAsync(CompletableFutureTest::fetchPriceSerial);
+        //成功后处理
+        cfFetch.thenAccept(System.out::println);
+        // 主线程不要立刻结束，否则CompletableFuture默认使用的线程池会立刻关闭:
+        Thread.sleep(2000);
+    }
+
+    /**
+     * @Description:
+     *          模拟场景二执行任务
+     * @Author Zepp Deng [2021-03-08 10:29]
+     * @param name
+     * @Return String
+     */
+    private static String queryCode(String name) {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "601857";
+    }
+
+    /**
+     * @Description:
+     *          模拟场景二执行任务
+     * @Author Zepp Deng [2021-03-08 11:01]
+     * @param code
+     * @Return Double
+     */
+    private static Double fetchPriceSerial(String code) {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return 5 + Math.random() * 20;
+    }
+
+    /**
+     * @Description:
+     *          场景三：并行两个任务，任意一个返回结果就算完成
+     * @Author Zepp Deng [2021-03-08 10:37]
+     */
+    public static void parallelAsync() throws InterruptedException {
+
+        // 两个CompletableFuture执行异步查询:
+        CompletableFuture<String> cfQueryFromSina = CompletableFuture.supplyAsync(() ->
+                queryCodeParallel("中国石油", "https://finance.sina.com.cn/code/"));
+        CompletableFuture<String> cfQueryFrom163 = CompletableFuture.supplyAsync(() ->
+                queryCodeParallel("中国石油", "https://money.163.com/code/"));
+        // 用anyOf合并为一个新的CompletableFuture
+        CompletableFuture<Object> cfQuery = CompletableFuture.anyOf(cfQueryFrom163,cfQueryFromSina);
+
+        // 两个CompletableFuture执行异步查询:
+        CompletableFuture<Double> cfFetchFromSina = cfQuery.thenApplyAsync((code) ->
+                fetchPriceParallel((String) code, "https://finance.sina.com.cn/price/"));
+        CompletableFuture<Double> cfFetchFrom163 = cfQuery.thenApplyAsync((code) ->
+                fetchPriceParallel((String) code, "https://money.163.com/price/"));
+        // 用anyOf合并为一个新的CompletableFuture:
+        CompletableFuture<Object> cfFetch = CompletableFuture.anyOf(cfFetchFromSina, cfFetchFrom163);
+
+        // 最终结果:
+        cfFetch.thenAccept(System.out::println);
+        // 主线程不要立刻结束，否则CompletableFuture默认使用的线程池会立刻关闭:
+        Thread.sleep(200);
+    }
+
+    private static String queryCodeParallel(String taskName, String url)
+    {
+        System.out.println("query code from " + url + "...");
+        try {
+            Thread.sleep((long) (Math.random() * 100));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "601857";
+    }
+
+    private static Double fetchPriceParallel(String code, String url) {
+        System.out.println("query price from " + url + "...");
+        try {
+            Thread.sleep((long) (Math.random() * 100));
+        } catch (InterruptedException e) {
         }
         return 5 + Math.random() * 20;
     }
